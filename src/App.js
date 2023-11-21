2a1e9ec08aa833d0cfa88c2832aa1314a4da8621
Dnci <bes.dan2017@ya.ru>
2023-11-21 17:35:47 +0300

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import AuthLoader from "./components/AuthLoader";
 import AuthScreen from "./components/AuthScreen";
 import CodeInput from "./components/CodeInput";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Routes} from "react-router-dom";
+import {Route} from "react-router-dom";
 const App = () => {
-   /* const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [emailDirty, setEmailDirty] = useState(false)
-    const [passwordDirty, setPasswordDirty] = useState(false)
-    const [emailError, setEmailError] = useState('Емейл не может быть пустым')
-    const [passwordError, setPasswordError] = useState('Пароль не может быть пустым')*/
   return (
-      <Router>
         <Routes>
           <Route path="/" element={<AuthLoader />} />
-            {<Route path="/auth-screen" element={<AuthScreen />} />}
-            {<Route path="/auth-screen/code-input" element={<CodeInput />} />}
+            <Route path="/auth-screen" element={<AuthScreen />} />
+            <Route path="/code-input" element={<CodeInput />} />
         </Routes>
-
-      </Router>
   );
 };
 
