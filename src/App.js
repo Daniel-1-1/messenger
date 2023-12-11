f7624b4f0eb9e79c67f0f1a0f07218f85091ab3b
Daniel-1-1 <bes.dan2017@ya.ru>
2023-12-11 17:01:04 +0300

    add MainPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import AuthLoader from "./components/AuthLoader";
 import AuthScreen from "./components/AuthScreen";
 import CodeInput from "./components/CodeInput";
+import MainPage from "./components/MainPage";
 import {Routes} from "react-router-dom";
 import {Route} from "react-router-dom";
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
           <Route path="/" element={<AuthLoader />} />
             <Route path="/auth-screen" element={<AuthScreen />} />
             <Route path="/code-input" element={<CodeInput />} />
+            <Route path="/main-page" element={<MainPage />} />
         </Routes>
   );
 };
