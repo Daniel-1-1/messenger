7e90202c07dad0844619e35d5ef5dd6bf2e6b90a
Daniel-1-1 <bes.dan2017@ya.ru>
2023-12-11 18:01:57 +0300

    featured/deleted chat lists

--- src/components/ChatListItem.js
+++ src/components/ChatListItem.js
@@ -1,16 +1,17 @@
+/*
 import React from "react";
 import styles from "../styles/ChatListItem.module.scss";
 
 const ChatListItem = ({ chat, onClick, isSelected }) => {
     return (
-        <div className={`${styles.chatListItem} ${isSelected ? styles.selected : ''}`} onClick={onClick}>
-            <img src={chat.profileIcon} alt="profileIcon" className={styles.profileIcon}/>
-            <div className={styles.chatDetails}>
-                <div className={styles.userName}>{chat.userName}</div>
-                <div className={styles.lastMessage}>{chat.lastMessage}</div>
-            </div>
-        </div>
+        // <div className={`${styles.chatListItem} ${isSelected ? styles.selected : ''}`} onClick={onClick}>
+        //     <img src={chat.profileIcon} alt="profileIcon" className={styles.profileIcon}/>
+        //     <div className={styles.chatDetails}>
+        //         <div className={styles.userName}>{chat.userName}</div>
+        //         <div className={styles.lastMessage}>{chat.lastMessage}</div>
+        //     </div>
+        // </div>
     );
 };
 
-export default ChatListItem;
+export default ChatListItem;*/
